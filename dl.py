39a7b3bb9eaa4219ca8a7c86ae1687b37621e0a4
Samuel Hiroyuki <samduasvezesmortal@gmail.com>
2022-03-03 00:38:32 -0300

    Update dl.py

--- dl.py
+++ dl.py
@@ -2,12 +2,12 @@ import sys, os
 from skillshare import Skillshare, splash
 
 # or by class ID:
-# dl = Skillshare("PHPSESSID=1c33be9de9fc2c0d588314a935770619")
+# dl.download_course_by_class_id('396830381')
 
 def main():
     dl = Skillshare("PHPSESSID=be16420cdd9790f57c0fbade47f805ac")
     course_url = sys.argv[1]
-    dl.download_course_by_class_id('396830381')
+    dl.download_course_by_url(course_url)
 
 
 if __name__ == "__main__":
